e6399902136b24366558114036912c0555b13d10
Juliana Polanco <julianapolanco1991@gmail.com>
2023-02-16 09:00:57 -0500

    Folder reorg

---
+++ Beginner/Project-7-2D-Maze-Solver/main.py
@@ -0,0 +1,45 @@
+#HURDLES 3
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+
+#HURDLES 4
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
