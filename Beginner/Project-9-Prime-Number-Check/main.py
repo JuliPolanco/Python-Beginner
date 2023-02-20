5c3736c260f9feb6ef7ec82c6ddf8b9049f87612
Juliana Polanco <julianapolanco1991@gmail.com>
2023-02-20 10:31:04 -0500

    Create main.py

---
+++ Beginner/Project-9-Prime-Number-Check/main.py
@@ -0,0 +1,22 @@
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print("The number is prime")
+    else:
+        print("The number is not prime")
+
+prime_checker(3)
+
+def is_prime(num):
+    for n in range(2, num):
+        if num % n == 0:
+            print("The number is not prime", n, "is divider")
+            return False
+    print("Es primo")
+    return True
+
+is_prime(13)
+is_prime(12)
