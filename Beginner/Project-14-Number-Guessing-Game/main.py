aea5e0c190cad6e2511f5e178d4ee842a177160e
Juliana Polanco <julianapolanco1991@gmail.com>
2023-03-10 08:54:28 -0500

    Folder reorg

---
+++ Beginner/Project-14-Number-Guessing-Game/main.py
@@ -0,0 +1,42 @@
+from random import randint
+from logoo import logo
+
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+def check_answer(guess, answer, turns):
+    if guess > answer:
+        print("Too high")
+        return turns - 1
+    elif guess < answer:
+        print("Too low")
+        return turns - 1
+    else:
+        print(f"You got the right answer! is {answer}")
+
+def set_difficulty():
+    level = input("chose the difficulty:'easy' or 'hard':")
+    if level == 'easy':
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+
+def game():
+    # print(logo)
+    print("Welcome to guess the number!")
+    print("I am thinking in a number between 1 and 100")
+    answer = randint(1, 100)
+    print(f"Psst! the correct answer is {answer}")
+    turns = set_difficulty()
+    guess = 0
+    while guess != answer:
+        print(f"You have {turns} attempts remaining to guess the number")
+        guess = int(input("Make a guess: "))
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print("You are out of attempts. You lose.")
+            return
+        elif guess != answer:
+            print("Guess again")
+
+game()
